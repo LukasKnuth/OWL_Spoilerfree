7872fed22e30f983cd54546bbd2097c43b4b007c
Lukas Knuth <mr.luke.187@googlemail.com>
2018-01-20 17:17:09 +0100

    Initial version which does a good job but isn't perfect.

---
+++ extension/src/hidePlayerSpoilers.js
@@ -0,0 +1,6 @@
+// iFrame with video-player loaded:
+$(document).on("load", () => {
+	$("div#player div#mlg-bottom-container div.controls-container-right div.mlg-time-display").text("Obmitted");
+});
+
+// TODO This doesn't trigger at all... Manifest flags should be fine...
---
+++ extension/src/hideSpoilers.js
@@ -0,0 +1,45 @@
+$("div.Wrap").livequery("div#schedule", (element) => {
+	// Hide results in the schedule
+	$(element).livequery("div.Schedule", (schedList) => {
+		$(schedList).livequery("div.MatchSchedule", (sched) => {
+			$(sched).find("div.MatchRow-match div.MatchRow-status > span.MatchStatus--transparent").text("Obmitted");
+		});
+	});
+}).livequery("div#match-details", (element) => {
+	// Hide results on the match-page
+	$(element).livequery("section.MatchOverview", (overview) => {
+		const scores = $(overview).find("div.TeamScore-score");
+		scores.find("svg").remove(); // remove the crone-icon
+		scores.find("span.TeamScore-scoreValue").text("Ø"); // remove the result-scores
+	}).livequery("section.MatchInfo", (info) => {
+		const mapCount = $(info).find("div.MatchInfo-item:nth-child(3)");
+		if (mapCount) {
+			mapCount.text("Map Count: Obmitted");
+		}
+	}).livequery("div.Tabs", (tabs) => {
+		const nav = $(tabs).find("nav.Tabs-bar");
+		// Remove any names of the maps played
+		nav.find("a.Tabs-tab").each((index, tab) => {
+			if (index > 0) {
+				$(tab).text("Map "+(index));
+			}
+		});
+		// Always add a fifth tab so that the map-count is unknown
+		if (nav.length < 6) {
+			nav.append('<a class="Tabs-tab" href="#">Map 5</a>')
+		}
+	}).livequery("section.MatchRecap", (recap) => {
+		// Remove all info-boxes.
+		$(recap).find("div.GameInfo").remove();
+		// Add info:
+		$(recap).find("h1.MatchRecap-title").text((i, oldContent) => {
+			return oldContent + " (Obmitted)";
+		})
+	}).livequery("div:nth-child(5)", (results) => {
+
+		// TODO this doesn't (always?) work!
+
+		// Remove the match-result below the player
+		$(results).find("div.GameResult div.MatchStatus").text("Obmitted");
+	});
+});
